616df9d58d3b5f829fef8d70fe130920725687ad
Error 404 - Name not found <bcr25939@kyois.com>
2017-05-18 09:05:45 -0400

    re-wrote main.py
    enabled command line argument support, decryption support, a lot of awkwardness

--- encryptor.py
+++ encryptor.py
@@ -1,11 +1,43 @@
-#ROT 13 with a twist (gettit?)
+import getopt, sys
 
-#Auth: Lysergic
-#Name: ROT13_stylized.py
-#Desc: Encrypts a string by using each character as a new key
- 
-def main():
-	original_message = raw_input("Enter a message: ")
+inputFile 	= ""
+outputFile 	= ""
+encrypt 	= False
+decrypt 	= False
+
+def readFile(inputFile):
+	message = ""
+	fi = open(inputFile, "r")
+	for line in fi:
+		message += line
+	return message
+
+def getMessage():
+	message = raw_input("Enter message    : ")
+	message += "\n"
+	return message
+
+def decryptMode():
+	global outputFile
+	global inputFile
+	original_message = ""
+
+	if not inputFile:
+		original_message = getMessage()
+	elif inputFile:
+		original_message = readFile(inputFile)
+
+def encryptMode():
+	global outputFile
+	global inputFile
+	original_message = ""
+
+	if not inputFile:
+		original_message = getMessage()
+	elif inputFile:
+		original_message = readFile(inputFile)
+
+
 	char_array = list(original_message)	#split input into idvidual characters
 	# print(char_array)	#debug output
 
@@ -53,6 +85,75 @@ def main():
 
 	# print(final_message_array)
 	final_message_string = "".join(final_message_array)
-	print("Encrypted message: {}").format(final_message_string) 	#output message
+
+	if outputFile:
+		fo = open(outputFile,"w")
+		fo.write(final_message_string)
+	else:
+		print("Encrypted message: {}").format(final_message_string) 	#output message
+
+def usage():
+	print
+	print("Usage: rot-13.py")
+	print("-h     displays this help message")
+	print("-e     sets rot-13.py to encrypt mode")
+	print("-d     sets rot-13.py to decrypt mode")
+	print("-i     input file for program to process (default is stdin)")
+	print("-o     outfile for program output (default is stdout)")
+	sys.exit(2)
+
+def main(argv):
+	#global variables
+	global inputFile
+	global outputFile
+	global encrypt
+	global decrypt
+
+	try:
+		opts, args = getopt.getopt(argv, "hedi:o:",)
+	except getopt.GetoptError as e:
+		print(str(e))
+		usage()
+
+	for opt, arg in opts:
+		if opt == "-h":
+			usage()
+
+		if opt == "-e":
+			encrypt = True
+
+		if opt == "-d":
+			decrypt = True
+
+		if opt == "-i":
+			inputFile = arg
+
+		if opt == "-o":
+			outputFile = arg
+
+	if not encrypt and not decrypt:
+		print("[!!] Error setting core variables:")
+		print("Encrypt mode: {}").format(encrypt)
+		print("Decrypt mode: {}").format(decrypt)
+		usage()
+
+	if encrypt and decrypt:
+		print("[!!] Error setting core variables:")
+		print("Encrypt mode: {}").format(encrypt)
+		print("Decrypt mode: {}").format(decrypt)
+		usage()
+
+	if not outputFile:
+		print("[*] no output file selected, outputting to terminal")
+	if not inputFile:
+		print("[*] no input file selected, input through stdin")
+
+	if encrypt:
+		encryptMode()
+	elif decrypt:
+		decryptMode()
+	else:
+		print("how on Earth did you manage that?")
+
 if __name__ == "__main__":
-	main()
+	main(sys.argv[1:])
